ca10dcb80da5c652f5da416d19bce636494b4d00
Paul Coroneos <paul@pcoroneos.com>
2020-09-23 23:39:55 -0500

    try again

--- express/server.js
+++ express/server.js
@@ -2,58 +2,61 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
-const serverless = require('serverless-http');
-const bodyParser = require('body-parser');
+const serverless = require("serverless-http");
+const bodyParser = require("body-parser");
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var router = express.Router();
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.use(bodyParser.json());
 
 // http://expressjs.com/en/starter/basic-routing.html
 router.get("/home", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
+// your first API endpoint...
 router.get("/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: "hello API" });
 });
 
-router.get("/api/timestamp/:date_string?", function(req,res) {
-
-  const dateParam = req.params['date_string'];
+router.get("/api/timestamp/:date_string?", function (req, res) {
+  const dateParam = req.params["date_string"];
 
   //date is empty
-  if(dateParam === undefined) {
+  if (dateParam === undefined) {
     const newDate = new Date();
-    return res.json({"unix": newDate.getTime(), "utc": newDate.toUTCString()})
+    return res.json({ unix: newDate.getTime(), utc: newDate.toUTCString() });
   }
 
   //date is invalid
   const date = Date.parse(dateParam);
-  if(isNaN(date)) {
-    res.json({"error": "Invalid Date"});
+  if (isNaN(date)) {
+    res.json({ error: "Invalid Date" });
   }
   //date valid, lets return formatted object
   else {
     //check if number > 5 to pass as num, otherwise pass as string
-    const parsedDate = /\d{5,}/.test(dateParam) ?  new Date(parseInt(dateParam)) : new Date(dateParam);
-    res.json({"unix": parsedDate.getTime(), "utc": parsedDate.toUTCString()})
+    let parsedDate;
+    if (/\d{5,}/.test(dateParam)) {
+      parsedDate = new Date(parseInt(dateParam));
+      res.json({ unix: dateParam, utc: parsedDate.toUTCString() });
+    } else {
+      parsedDate = new Date(dateParam);
+      res.json({ unix: parsedDate.getTime(), utc: parsedDate.toUTCString() });
+    }
   }
-
 });
 
-app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use("/.netlify/functions/server", router); // path must route to lambda
 
 module.exports = app;
 module.exports.handler = serverless(app);
